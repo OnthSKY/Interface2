7ef1f4522dc74862f0dd2a4838d761437424f2e6
SKY <onthsky@gmail.com>
2020-05-17 15:50:48 +0300

    Add project files.

---
+++ Interface2/ICustomerDal.cs
@@ -0,0 +1,56 @@
+﻿using Inteface2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interface2
+{
+    interface ICustomerDal //Dal --> Veri tabani islemlerini yapmak icin 
+    {
+        void Add();
+        void Update();
+        void Delete();
+    }
+    class OracleCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Oracle Added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Oracle Deleted");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Oracle Updated");
+        }
+    }
+    class SqlServerCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Sql Added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Sql Deleted");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Sql Updated");
+        }
+    }
+
+    class CustomerManager
+    {
+        public void Add(ICustomerDal customerDal)
+        {
+            customerDal.Add();
+        }
+    }
+}
---
+++ Interface2/Program.cs
@@ -0,0 +1,75 @@
+﻿using Interface2;
+using System;
+
+namespace Inteface2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //InterfaceIntro();
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(new SqlServerCustomerDal());
+            
+            Console.ReadLine();
+        }
+
+        private static void InterfaceIntro()
+        {
+            PersonManager manager = new PersonManager();
+            Customer customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Kemal",
+                LastName = " Yogurt",
+                Address = "Ankara"
+            };
+            manager.Add(customer);
+            // manager.Add(new customer{Id = 1, FirstName = "Kemal",LastName = " Yogurt", Address = "Ankara"}); şeklinde de eklenebilir ... 
+            Student student = new Student
+            {
+                Id = 2,
+                FirstName = "Dilanur",
+                LastName = " Yogurt",
+
+            };
+            manager.Add(student);
+        }
+    }
+
+
+    interface IPerson // soyut nesne 
+    {
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+    }
+    class Customer : IPerson            //somut nesne (:IPerson yapilmasinin nedeni IPerson'da tanimlanana her seyi sende kullanabilmesi 
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+    }
+    class Student : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Departmant { get; set; }
+    }
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine("Id :" + person.Id);
+            Console.WriteLine("First Name :{0} ", person.FirstName);
+            Console.WriteLine("Last Name :" + person.LastName);
+
+
+
+        }
+    }
+}
